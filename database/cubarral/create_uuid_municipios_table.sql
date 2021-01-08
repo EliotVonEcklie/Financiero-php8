1875a3632edc1317a26108564b407bf3fae04ca5
Joseph Hans Diaz Gonzalez <josephhansdiaz30@gmail.com>
2021-01-08 15:09:32 -0500

    Create SQL Scripts for database updates

---
+++ database/cubarral/create_uuid_municipios_table.sql
@@ -0,0 +1,10 @@
+-- cubarral.uuid_municipios definition
+
+CREATE TABLE `uuid_municipios` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(37) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `slogan` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uuid_municipios_un` (`uuid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
---
+++ database/cubarral/update_contabilidad_module_path.sql
@@ -0,0 +1,7 @@
+--  Auto-generated SQL script #202101081507
+
+-- Updates the path of the 'contabilidad' module.
+
+UPDATE cubarral.modulos
+	SET libre='modules/contabilidad/cont-principal.php'
+	WHERE id_modulo=1;
